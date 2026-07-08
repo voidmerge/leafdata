d9789ad1ef86a922462dab9be978e36b39ec539c
neonphog <neonphog@gmail.com>
2026-07-08 16:11:23 -0600

    wip

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,16 +1,20 @@
-//import { Render, Parse } from './index.js';
 import { LeafObj, LeafArr } from './index.js';
 
 describe('leaf-data', async () => {
   it('stringify sanity', async () => {
+    const inner = new LeafObj()
+      .withBool('test', true, [' second note'])
+      .withProp('arr', new LeafArr().pushBool(true))
+      .withBin('b1', new TextEncoder().encode('hello"').buffer)
+      .withBin('b2', new Uint8Array([0, 1, 2, 3, 4, 5]).buffer)
+      .withBin('b3', new Uint8Array(180).buffer)
+      .withF64('num', 3.141)
+      .withBigInt('big', 94878374687368437684n);
+    inner.tailNotes = [' nother tail'];
     const obj = new LeafObj()
-      .withBool('test', true, ['note test'])
-      .withProp(
-        'obj',
-        new LeafObj()
-          .withBool('test', true, ['second note'])
-          .withProp('arr', new LeafArr().pushBool(true)),
-      );
+      .withBool('test', true, [' note test'])
+      .withProp('obj', inner);
+    obj.tailNotes = [' test tail note'];
 
     //console.log(JSON.stringify(obj));
 
@@ -22,47 +26,42 @@ obj = {
   arr = [
     true
   ]
+  b1 = b@"hello%22"
+  b2 = B@"AAECAwQF"
+  b3 = B@"
+    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+    AAAAAAAAAAAAAAAAAA"
+  num = 3.141
+  big = n@"94878374687368437684"
+  # nother tail
 }
+# test tail note
 `);
   });
 
   it('parse sanity', async () => {
     const obj = LeafObj.parse(`
+# top
 obj = {
+  # inner
   test = bob
   ned = true
-  "fred" = "bob"
+  "fred" = "bob
+multi"
+  zorb = null
+  num = 3.141
+  big = n@"94878374687368437684"
+  b1 = b@"hell
+o%22"
+  b2 = B@"A
+    AE
+    CA
+    wQF"
+  # tail
 }
 `);
     console.log(JSON.stringify(obj, null, 2));
-    /*
-    const p = new Parse();
-    p.writeChunk(`
-foo = {
-  hello="world"
-  bool=true
-  "neddo"=false
-}
-`);
-    for (const leaf of p) {
-      console.log(JSON.stringify(leaf));
-    }
-    */
-
-    /*
-    const pos = { line: 1, col: 1 };
-
-    for (const chunk of [
-      '{',
-      '  // what up?\n',
-      '  t:@t, // thing\n',
-      '  f:@f,\n  n:@n,\n',
-      '  num:3.141,\n',
-      '}',
-    ]) {
-      const res = tokenize(chunk, pos);
-      console.error(JSON.stringify(res));
-    }
-    */
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
 export enum LeafNodeType {
-  Null = 'Null',
-  Obj = 'Obj',
-  Arr = 'Arr',
-  Bool = 'Bool',
-  Str = 'Str',
+  Null = 'null',
+  Bool = 'bool',
+  Str = 'str',
+  F64 = 'f64',
+  BigInt = 'bigint',
+  Obj = 'obj',
+  Arr = 'arr',
+  Bin = 'bin',
 }
 
 export class LeafNode {
   readonly t: LeafNodeType = LeafNodeType.Null;
   notes?: string[];
+  loc?: [string, string];
 
   constructor(notes?: string[]) {
     if (notes) {
@@ -27,6 +31,7 @@ export class LeafNode {
 
 export class LeafObj extends LeafNode {
   readonly t = LeafNodeType.Obj;
+  tailNotes?: string[];
 
   v: { [prop: string]: LeafVal };
 
@@ -46,6 +51,18 @@ export class LeafObj extends LeafNode {
 
   withStr(k: string, v: string, notes?: string[]): LeafObj {
     return this.withProp(k, new LeafStr(v, notes));
+  }
+
+  withBin(k: string, v: ArrayBuffer, notes?: string[]): LeafObj {
+    return this.withProp(k, new LeafBin(v, notes));
+  }
+
+  withF64(k: string, v: number, notes?: string[]): LeafObj {
+    return this.withProp(k, new LeafF64(v, notes));
+  }
+
+  withBigInt(k: string, v: bigint, notes?: string[]): LeafObj {
+    return this.withProp(k, new LeafBigInt(v, notes));
   }
 
   stringify(): string {
@@ -75,6 +92,14 @@ export class LeafArr extends LeafNode {
   }
 }
 
+export class LeafNull extends LeafNode {
+  readonly t = LeafNodeType.Null;
+
+  constructor(notes?: string[]) {
+    super(notes);
+  }
+}
+
 export class LeafBool extends LeafNode {
   readonly t = LeafNodeType.Bool;
 
@@ -97,7 +122,72 @@ export class LeafStr extends LeafNode {
   }
 }
 
-export type LeafVal = LeafObj | LeafArr | LeafBool | LeafStr;
+export class LeafBin extends LeafNode {
+  readonly t = LeafNodeType.Bin;
+
+  v: ArrayBuffer;
+
+  constructor(v: ArrayBuffer, notes?: string[]) {
+    super(notes);
+    this.v = v;
+  }
+
+  toJSON(): any {
+    const out: any = { t: this.t };
+    if (this.notes) {
+      out.notes = this.notes;
+    }
+    if (this.loc) {
+      out.loc = this.loc;
+    }
+    out.v = renderBin(this.v);
+    return out;
+  }
+}
+
+export class LeafF64 extends LeafNode {
+  readonly t = LeafNodeType.F64;
+
+  v: number;
+
+  constructor(v: number, notes?: string[]) {
+    super(notes);
+    this.v = v;
+  }
+}
+
+export class LeafBigInt extends LeafNode {
+  readonly t = LeafNodeType.BigInt;
+
+  v: bigint;
+
+  constructor(v: bigint, notes?: string[]) {
+    super(notes);
+    this.v = v;
+  }
+
+  toJSON(): any {
+    const out: any = { t: this.t };
+    if (this.notes) {
+      out.notes = this.notes;
+    }
+    if (this.loc) {
+      out.loc = this.loc;
+    }
+    out.v = this.v.toString();
+    return out;
+  }
+}
+
+export type LeafVal =
+  | LeafObj
+  | LeafArr
+  | LeafNull
+  | LeafBool
+  | LeafStr
+  | LeafBin
+  | LeafF64
+  | LeafBigInt;
 
 function renderVal(out: string[], depth: number, val: LeafVal) {
   switch (val.t) {
@@ -112,6 +202,20 @@ function renderVal(out: string[], depth: number, val: LeafVal) {
       break;
     case LeafNodeType.Str:
       renderStr(out, val.v);
+      break;
+    case LeafNodeType.Bin:
+      const [t, s] = renderBin(val.v, depth);
+      if (t === 'pct') {
+        out.push(`b@${JSON.stringify(s)}`);
+      } else {
+        out.push(`B@"${s}"`);
+      }
+      break;
+    case LeafNodeType.F64:
+      out.push(JSON.stringify(val.v));
+      break;
+    case LeafNodeType.BigInt:
+      out.push(`n@"${val.v.toString()}"`);
       break;
   }
 }
@@ -130,6 +234,72 @@ function renderStr(out: string[], str: string) {
   }
 }
 
+function renderBinB64(b: ArrayBuffer, depth?: number): string {
+  const bytes = new Uint8Array(b);
+  const binary = [];
+
+  for (let i = 0; i < bytes.byteLength; i++) {
+    binary.push(String.fromCharCode(bytes[i]));
+  }
+
+  let result = btoa(binary.join(''));
+
+  if (depth) {
+    depth += 1;
+
+    let limit = 78 - depth * 2;
+    if (limit < 64) {
+      limit = 64;
+    }
+
+    if (result.length > limit) {
+      const re = new RegExp(`.{1,${limit}}`, 'g');
+      const chunks: string[] = result.match(re) || [];
+
+      chunks.unshift('');
+      result = chunks.join('\n' + '  '.repeat(depth));
+    }
+  }
+
+  return result;
+}
+
+function renderBinPct(b: ArrayBuffer): string {
+  const bytes = new Uint8Array(b);
+  const result = [];
+
+  for (let i = 0; i < bytes.length; i++) {
+    const byte = bytes[i];
+    if (
+      (byte >= 0x00 && byte <= 0x1f) || // control characters
+      byte == 0x22 || // "
+      byte == 0x25 || // %
+      byte == 0x5c || // \
+      (byte >= 0x7f && byte <= 0xff) // extended ascii
+    ) {
+      // we need to escape this character
+      const hex = byte.toString(16).toUpperCase().padStart(2, '0');
+      result.push(`%${hex}`);
+    } else {
+      // this character is allowed
+      result.push(String.fromCharCode(byte));
+    }
+  }
+
+  return result.join('');
+}
+
+function renderBin(b: ArrayBuffer, depth?: number): ['pct' | 'b64', string] {
+  const pct = renderBinPct(b);
+  const b64 = renderBinB64(b, depth);
+
+  if (b64.length < pct.length) {
+    return ['b64', b64];
+  }
+
+  return ['pct', pct];
+}
+
 function renderObjProps(out: string[], depth: number, obj: LeafObj) {
   for (const prop in obj.v) {
     const v = obj.v[prop];
@@ -137,7 +307,7 @@ function renderObjProps(out: string[], depth: number, obj: LeafObj) {
     if (v.notes) {
       for (const note of v.notes) {
         renderDepth(out, depth);
-        out.push(`# ${note}\n`);
+        out.push(`#${note}\n`);
       }
     }
 
@@ -146,6 +316,12 @@ function renderObjProps(out: string[], depth: number, obj: LeafObj) {
     out.push(' = ');
     renderVal(out, depth, v);
     out.push('\n');
+  }
+  if (obj.tailNotes) {
+    for (const note of obj.tailNotes) {
+      renderDepth(out, depth);
+      out.push(`#${note}\n`);
+    }
   }
 }
 
@@ -162,7 +338,7 @@ function renderArr(out: string[], depth: number, arr: LeafArr) {
     if (v.notes) {
       for (const note of v.notes) {
         renderDepth(out, depth + 1);
-        out.push(`# ${note}\n`);
+        out.push(`#${note}\n`);
       }
     }
 
@@ -174,20 +350,59 @@ function renderArr(out: string[], depth: number, arr: LeafArr) {
   out.push(']');
 }
 
+function parsePct(pct: string): ArrayBuffer {
+  const tokenMatcher = /%[0-9a-fA-F]{2}|[\s\S]/g;
+  const matches = pct.match(tokenMatcher) || [];
+
+  const byteArray = new Uint8Array(matches.length);
+
+  for (let i = 0; i < matches.length; i++) {
+    const token = matches[i];
+
+    if (token.startsWith('%')) {
+      byteArray[i] = parseInt(token.substring(1), 16);
+    } else {
+      byteArray[i] = token.charCodeAt(0);
+    }
+  }
+
+  return byteArray.buffer;
+}
+
+function parseB64(b64: string): ArrayBuffer {
+  const binaryString = atob(b64);
+  const len = binaryString.length;
+
+  const bytes = new Uint8Array(len);
+
+  for (let i = 0; i < len; i++) {
+    bytes[i] = binaryString.charCodeAt(i);
+  }
+
+  return bytes.buffer;
+}
+
 const RE: [string, RegExp][] = [
   ['whitespace', /([, \t\r\n]+)/y],
   ['{', /(\{)/y],
   ['}', /(\})/y],
   ['[', /(\[)/y],
   [']', /(\])/y],
+  ['note', /#(.*)/my],
+  ['null', /null\b/y],
   ['bool', /(true|false)\b/y],
+  ['bigint', /n@("-?(?:0|[1-9]\d*)")/y],
+  ['bin-pct', /b@("(?:[^"\\]|\\["\\\/bfnrt]|\\u[0-9a-fA-F]{4})*")/y],
+  ['bin-b64', /B@("(?:[^"\\]|\\["\\\/bfnrt]|\\u[0-9a-fA-F]{4})*")/y],
   ['prop-id', /([a-zA-Z_$][a-zA-Z0-9_$]{0,63})[ \t\r\n]*=/y],
   ['prop-str', /("(?:[^"\\]|\\["\\\/bfnrt]|\\u[0-9a-fA-F]{4})*")[ \t\r\n]*=/y],
   ['id', /([a-zA-Z_$][a-zA-Z0-9_$]{0,63})/y],
   ['str', /("(?:[^"\\]|\\["\\\/bfnrt]|\\u[0-9a-fA-F]{4})*")/y],
+  ['f64', /(-?(?:0|[1-9]\d*)(?:\.\d+)?(?:[eE][+-]?\d+)?)/y],
 ];
 
-type Tok = [string, string, string | boolean, string, string];
+type TokVal = null | string | boolean | number | bigint | ArrayBuffer;
+type Tok = [string, string, TokVal, string, string];
 
 function lex(data: string): Tok[] {
   const out: Tok[] = [];
@@ -215,12 +430,29 @@ function lex(data: string): Tok[] {
           }
         }
 
-        let value = match[1];
+        let value: TokVal = match[1];
         switch (t) {
-          case 'prop-str':
+          case 'null':
+            value = null;
+            break;
           case 'str':
+          case 'prop-str':
+            value = JSON.parse(value.replaceAll('\n', '\\n'));
+            break;
           case 'bool':
+          case 'f64':
             value = JSON.parse(value);
+            break;
+          case 'bigint':
+            value = BigInt(JSON.parse(value));
+            break;
+          case 'bin-pct':
+            value = value.replaceAll('\n', '\\n');
+            value = parsePct(JSON.parse(value));
+            break;
+          case 'bin-b64':
+            value = value.replaceAll(/[ \t\r\n]+/g, '');
+            value = parseB64(JSON.parse(value));
             break;
         }
 
@@ -260,32 +492,75 @@ function peekTok(tok: Tok[], want: string[]): Tok {
 }
 
 function parseVal(tok: Tok[]): LeafVal {
-  const val = peekTok(tok, ['{', 'id', 'str', 'bool']);
+  const val = peekTok(tok, [
+    '{',
+    'null',
+    'id',
+    'str',
+    'bin-pct',
+    'bin-b64',
+    'bool',
+    'f64',
+    'bigint',
+  ]);
   tok.shift();
+  let out: LeafVal = new LeafNull();
   switch (val[0]) {
     case '{':
       return parseObj(tok);
+    case 'null':
+      out.loc = [val[3], val[4]];
+      return out;
     case 'id':
     case 'str':
-      return new LeafStr(val[2] as string);
+      out = new LeafStr(val[2] as string);
+      out.loc = [val[3], val[4]];
+      return out;
+    case 'bin-pct':
+    case 'bin-b64':
+      out = new LeafBin(val[2] as ArrayBuffer);
+      out.loc = [val[3], val[4]];
+      return out;
     case 'bool':
-      return new LeafBool(val[2] as boolean);
+      out = new LeafBool(val[2] as boolean);
+      out.loc = [val[3], val[4]];
+      return out;
+    case 'f64':
+      out = new LeafF64(val[2] as number);
+      out.loc = [val[3], val[4]];
+      return out;
+    case 'bigint':
+      out = new LeafBigInt(val[2] as bigint);
+      out.loc = [val[3], val[4]];
+      return out;
   }
   throw new Error('unreachable');
 }
 
 function parseObjProps(tok: Tok[], obj: LeafObj, isTop?: boolean) {
+  let notes = [];
   while (true) {
     if (isTop === true && !tok.length) {
-      return;
+      break;
     }
-    const key = peekTok(tok, ['prop-id', 'prop-str', '}']);
+    const key = peekTok(tok, ['note', 'prop-id', 'prop-str', '}']);
     if (key[0] === '}') {
-      return;
+      break;
     }
     tok.shift();
+    if (key[0] === 'note') {
+      notes.push(key[2] as string);
+      continue;
+    }
     const val = parseVal(tok);
+    if (notes.length) {
+      val.notes = notes;
+      notes = [];
+    }
     obj.withProp(key[2] as string, val);
+  }
+  if (notes.length) {
+    obj.tailNotes = notes;
   }
 }
 
@@ -299,150 +574,3 @@ function parseObj(tok: Tok[]): LeafObj {
 
   return obj;
 }
-
-/*
-export enum LeafType {
-  Ws = 'Ws',
-  Prop = 'Prop',
-  ObjBeg = 'ObjBeg',
-  ObjEnd = 'ObjEnd',
-  ArrBeg = 'ArrBeg',
-  ArrEnd = 'ArrEnd',
-  Bool = 'Bool',
-  Str = 'Str',
-}
-export type LeafLocStart = string;
-export type LeafLocEnd = string;
-export type LeafRepr = string;
-export type LeafVal = null | number | boolean | string;
-export type Leaf = [LeafType, LeafRepr, LeafVal, LeafLocStart?, LeafLocEnd?];
-
-export class Render {
-  #out: Leaf[];
-
-  constructor() {
-    this.#out = [];
-  }
-
-  *[Symbol.iterator](): Iterator<Leaf> {
-    for (let out = this.#out.shift(); out; out = this.#out.shift()) {
-      if (out) {
-        yield out;
-      } else {
-        break;
-      }
-    }
-  }
-
-  render(): string {
-    const out = [];
-    for (const [_, repr] of this) {
-      out.push(repr);
-    }
-    return out.join('');
-  }
-
-  prop(key: string) {
-    this.#out.push([LeafType.Prop, `${key}=`, key]);
-  }
-
-  str(str: string) {
-    this.#out.push([LeafType.Str, JSON.stringify(str), str]);
-    this.#out.push([LeafType.Ws, '\n', null]);
-  }
-
-  bool(bool: boolean) {
-    this.#out.push([LeafType.Bool, bool.toString(), bool]);
-    this.#out.push([LeafType.Ws, '\n', null]);
-  }
-
-  objBeg() {
-    this.#out.push([LeafType.ObjBeg, '{', null]);
-    this.#out.push([LeafType.Ws, '\n', null]);
-  }
-
-  objEnd() {
-    this.#out.push([LeafType.ObjEnd, '}', null]);
-    this.#out.push([LeafType.Ws, '\n', null]);
-  }
-
-  arrBeg() {
-    this.#out.push([LeafType.ArrBeg, '[', null]);
-    this.#out.push([LeafType.Ws, '\n', null]);
-  }
-
-  arrEnd() {
-    this.#out.push([LeafType.ArrEnd, ']', null]);
-    this.#out.push([LeafType.Ws, '\n', null]);
-  }
-}
-
-const reList: [LeafType, RegExp][] = [
-  [LeafType.Ws, /^[, \t\r\n]+/],
-  [LeafType.Prop, /^([a-zA-Z_$][a-zA-Z0-9_$]*)[ \t\r\n]*=/],
-  [
-    LeafType.Prop,
-    /^"((?:[^"\\]|\\["\\\/bfnrt]|\\u[0-9a-fA-F]{4})*)"[ \t\r\n]*=/,
-  ],
-  [LeafType.ObjBeg, /^\{/],
-  [LeafType.ObjEnd, /^\}/],
-  [LeafType.ArrBeg, /^\[/],
-  [LeafType.ArrEnd, /^\]/],
-  [LeafType.Bool, /^(?:true|false)\b/],
-  [LeafType.Str, /^"(?:[^"\\]|\\["\\\/bfnrt]|\\u[0-9a-fA-F]{4})*"/],
-];
-
-export class Parse {
-  #input: string;
-  #out: Leaf[];
-
-  constructor() {
-    this.#input = '';
-    this.#out = [];
-  }
-
-  *[Symbol.iterator](): Iterator<Leaf> {
-    while (true) {
-      let didMatch = false;
-      for (const [type, re] of reList) {
-        const match = this.#input.match(re);
-
-        if (match) {
-          const value = match[0];
-          this.#input = this.#input.slice(value.length);
-
-          switch (type) {
-            case LeafType.Prop:
-              yield [type, value, match[1]];
-              break;
-            case LeafType.Str:
-              yield [type, value, JSON.parse(value)];
-              break;
-            case LeafType.Bool:
-              yield [type, value, JSON.parse(value)];
-              break;
-            default:
-              yield [type, value, null];
-              break;
-          }
-
-          didMatch = true;
-        }
-      }
-      if (!didMatch) {
-        return;
-      }
-    }
-  }
-
-  parse(): Leaf[] {
-    const out = this.#out;
-    this.#out = [];
-    return out;
-  }
-
-  writeChunk(chunk: string) {
-    this.#input = this.#input + chunk;
-  }
-}
-*/
